aa33a194ae14ba1d175e7196d3e8fb982828391a
Nikolas Burk <nikolas.burk@gmail.com>
2017-02-04 17:32:21 +0100

    added Highlight component

--- src/Beer.js
+++ src/Beer.js
@@ -7,18 +7,30 @@ class Beer extends Component {
   render() {
     return (
       <div className="Beer-container">
-        <div className="Beer-name">{this.props.hit.name}</div>
-        <div className="Beer-attribute">{this.props.hit.style}</div>
-        <div className="Beer-attribute">{this.props.hit.brewery.name}</div>
-        <div className="Beer-attribute-number">{this.props.hit.ibu || '-'}</div>
-        <div className="Beer-attribute-number">{this.alcohol()} %</div>
+        <div className="Beer-name">
+          <Highlight attributeName='name' hit={this.props.hit}>{this.props.hit.name}</Highlight>
+        </div>
+        <div className="Beer-attribute">
+          <Highlight attributeName='style' hit={this.props.hit}>{this.props.hit.style}</Highlight>
+        </div>
+        <div className="Beer-attribute">
+          <Highlight attributeName='brewery.name' hit={this.props.hit}>{this.props.hit.brewery.name}</Highlight>
+        </div>
+        <div className="Beer-attribute-number">
+          {this.props.hit.ibu || '-'}
+        </div>
+        <div className="Beer-attribute-number">
+          {this.alcohol()} %
+        </div>
       </div>
     )
   }
 
+
   alcohol() {
-    const alcolholString = String(this.props.hit.abv * 100)
-    return alcolholString.substring(0, 3)
+    const alcolholString = String(this.props.hit.abv * 100).substring(0, 3)
+    console.log(alcolholString)
+    return alcolholString
   }
 }
 
